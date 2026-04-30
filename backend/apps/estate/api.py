86b2c5adc7d8006cc136efc49337c4e7049b7ba7
Fabian Palmera <fapalmera@cenditel.gob.ve>
2026-04-30 14:05:28 -0400

    agregar api backend de municipios y parroquias

--- backend/apps/estate/api.py
+++ backend/apps/estate/api.py
@@ -1,6 +1,7 @@
 from ninja import Router, Schema
-from typing import List
-from .models import Estate
+from typing import List, Optional
+from django.shortcuts import get_object_or_404
+from .models import Estate, Municipality, Parish
 
 router = Router()
 
@@ -11,6 +12,20 @@ class EstateSchema(Schema):
     name: str
     code: str
 
+class MunicipalitySchema(Schema):
+    """Schema basado en el modelo para devolver datos (Output)"""
+    id: int
+    name: str
+    code: str
+    estate: Optional[EstateSchema] = None
+
+class ParishSchema(Schema):
+    """Schema basado en el modelo para devolver datos (Output)"""
+    id: int
+    name: str
+    code: str
+    municipality: Optional[MunicipalitySchema] = None
+
 # --- ENDPOINTS (CRUD) ---
 
 # 1. Listar estados (GET)
@@ -18,12 +33,28 @@ class EstateSchema(Schema):
 def list_estate(request):
     return Estate.objects.all()
 
-# 1. Listar municipios (GET)
-# @router.get("/municipalities", response=List[MunicipalitySchema])
-# def list_municipality(request):
-#     return Municipality.objects.select_related("estate")
+# 2. Listar municipios (GET)
+@router.get("/municipalities", response=List[MunicipalitySchema])
+def list_municipality(request):
+    return Municipality.objects.select_related("estate")
 
-# 1. Listar parroquias (GET)
-# @router.get("/parishes", response=List[ParishSchema])
-# def list_parish(request):
-#     return Parish.objects.select_related("municipality")
+# 3. Listar municipios que pertenecen al estado (GET)
+@router.get("/municipalities/{estate_id}", response=List[MunicipalitySchema])
+def get_municipalities_by_estate(request, estate_id):
+    # Access the estate object or its ID
+    current_estate_code = get_object_or_404(Estate, pk=estate_id).code
+
+    return Municipality.objects.filter(estate_id=current_estate_code)
+
+# 2. Listar parroquias (GET)
+@router.get("/parishes", response=List[ParishSchema])
+def list_parish(request):
+    return Parish.objects.select_related("municipality")
+
+# 3. Listar parroquias que pertenecen al municipio (GET)
+@router.get("/parishes/{municipality_id}", response=List[ParishSchema])
+def get_parishes_by_municipality(request, municipality_id):
+    # Access the municipality object or its ID
+    current_municipality_code = get_object_or_404(Municipality, pk=municipality_id).code
+
+    return Parish.objects.filter(municipality_id=current_municipality_code)
